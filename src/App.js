6340c4538e95719095f7181dab35ce0370b0219d
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2018-03-17 14:14:32 +0100

    A sample solution for exercise 1

--- src/App.js
+++ src/App.js
@@ -1,90 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const teams = [
-  {
-    name: 'Carriers',
-    pms: ['Javier'],
-    pds: ['Nacho'],
-    devs: ['Alvar', 'David', 'Dmitri', 'Iván', 'Martin', 'Vadim'],
-  },
-  {
-    name: 'Shippers',
-    pms: ['Isa', 'Joa'],
-    pds: ['Clau', 'Óscar'],
-    devs: ['Alix', 'Antonio', 'H3', 'James', 'Nasi', 'Ricardo', 'Sotos'],
-  },
-  {
-    name: 'Ops',
-    pms: ['Ramón'],
-    pds: ['Carlos'],
-    devs: ['Acei', 'Cas', 'Jose', 'Juan', 'Juanjo', 'Rach', 'Rafa', 'Rubén'],
-  },
-];
+import space from './peopleinspace.json';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to OnTruck Engineering</h1>
+          <h1>There are <strong>{space.number}</strong> people in space right now</h1>
         </header>
-        <p className="App-intro"></p>
-        <p className="Teams">
-          We have { teams.length } teams:
-          <ul>
-          { teams.map( ({ name, pms, pds, devs }) =>
-            <li key={name}>
-              Team { name } has { pms.length } PMs, { pds.length } PDs and { devs.length } Devs
-            </li>
+        <p className="People">
+          { space.people.map(person =>
+            <div class="Person" key={person.name}>
+              <img class="Pic" src={person.biophoto} width={person.biophotowidth} height={person.biophotoheight} />
+              <div class="Info">
+                <div>{ person.title } { person.name } from <img title={person.country} class="Flag" src={person.countryflag} /></div>
+                <div>At "{ person.location }" since { person.launchdate }</div>
+                <div><em>{ person.bio }</em></div>
+                <div class="Links">
+                  { person.biolink ? <a href={person.biolink} target="_blank" rel="noopener">Bio</a> : null }
+                  { person.twitter ? <a href={person.twitter} target="_blank" rel="noopener">Twitter</a> : null }
+                </div>
+              </div>
+            </div>
           )}
-          </ul>
-        </p>
-        <p className="Teams">
-          We have { teams.length } teams, sorted by number of developers:
-          <ul>
-          { teams.sort((a, b) => a.devs.length < b.devs.length ).map( ({ name, pms, pds, devs }) =>
-            <li key={name}>
-              Team { name } has { pms.length } PMs, { pds.length } PDs and { devs.length } Devs
-            </li>
-          )}
-          </ul>
-        </p>
-        <p className="Teams">
-          We have { teams.filter(team => team.pms.length > 1 ).length } team(s) with more than one PM, we need to split them:
-          <ul>
-          { teams.filter(team => team.pms.length > 1 ).map( ({ name, pms, pds, devs }) =>
-            <li key={name}>
-              Team { name } has { pms.length } PMs, { pds.length } PDs and { devs.length } Devs
-            </li>
-          )}
-          </ul>
-        </p>
-        <p className="Teams">
-          { teams.every(team => team.pds.length >= 1) ?
-            <span>Hooray! Every team has at least one PD! We are safe!</span> :
-            <span>Oh no! Some teams do not have a PD! Go hire some more!</span>
-          }
-        </p>
-        <p className="Teams">
-          We have a total of:
-          <ul>
-          { ['pms', 'pds', 'devs'].map(position =>
-            <li key={position}>
-              {
-                teams
-                .map(team => team[position].length)
-                .reduce((a, b) => a+b, 0)
-              } { position.slice(0, -1).toUpperCase() }{ position.slice(-1) }
-            </li>
-          )}
-          </ul>
-          For a grand total of { teams
-            .map( ({ pms, pds, devs }) => [ ...pms, ...pds, ...devs ].length )
-            .reduce((a, b) => a+b, 0)
-          } team members
         </p>
       </div>
     );
